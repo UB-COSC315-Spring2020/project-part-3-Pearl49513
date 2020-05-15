92d01da3c63b8989ca10d2397d8217a6d8a50751
Pearl49513 <dahlia.casey@ubalt.edu>
2020-05-15 19:54:42 -0400

    EditPart1
    EditPart1

---
+++ CaseyDahlia_COSC315_FinalProject_Part3/Assets/Scripts/TopDownCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    //Focus Target Character
+    public GameObject target;
+    public Vector3 offset = new Vector3(0, 0, -1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (target)
+        {
+            //Always focuses exactually to the character when the player is moving
+            transform.position = new Vector3(
+                target.transform.position.x + offset.x,
+                target.transform.position.y + offset.y,
+                target.transform.position.z + offset.z);
+        }
+    }
+}
