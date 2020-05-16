be6a32cdac5d2453d008407597d0ea642c160295
Pearl49513 <dahlia.casey@ubalt.edu>
2020-05-15 20:03:18 -0400

    EditPart3
    EditPart3

---
+++ CaseyDahlia_COSC315_FinalProject_Part3/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider loadingSlider;
+    public Text progressText;
+
+    public void PlayGame(int sceneIndex)
+    {
+        //Player plays a new game aka a new save
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            
+            loadingSlider.value = progress;
+
+            progressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
+
+    public void QuitGame()
+    {
+        //Player quits the game and the application closes
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
---
+++ CaseyDahlia_COSC315_FinalProject_Part3/Assets/Scripts/MainMenu/OptionsMenu/OptionMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionMenu : MonoBehaviour
+{
+
+    public AudioMixer audioOption;
+    public void SetBackGroundMusic (float volumeBGM)
+    {
+        Debug.Log(volumeBGM);
+        audioOption.SetFloat("volumeBGM", volumeBGM);
+    }
+
+    public void SetSoundEffect (float volumeSE)
+    {
+        Debug.Log(volumeSE);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        Debug.Log("Is fullscreen");
+    }
+
+    public void SetWindow (bool isWindowscreen)
+    {
+        Screen.SetResolution(800, 600, false);
+        Debug.Log("Is windowed");
+    }
+  
+
+
+}
